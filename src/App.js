91644cb5a2aa943c3b1016f42355985c470b21ff
ericbowman <eric@lazarus.enterprises>
2020-02-20 14:37:58 -0500

    setup for deploy

--- src/App.js
+++ src/App.js
@@ -1,272 +1,291 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="name-container">
-        <svg
-          className="name"
-          xmlns="http://www.w3.org/2000/svg"
-          width="373.899"
-          height="203.001"
-          x="0"
-          y="0"
-          version="1.1"
-          viewBox="0 0 373.899 203.001"
-          xmlSpace="preserve"
-        >
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M39.4 41.066L63 41.066"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M39.352 61.066L62.951 61.066"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M94.05 21.11L94.05 35.69"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M94.05 78.4L94.05 101.5"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M193.809 27.871L193.809 74.871"
-            className="line2"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M194.051 51L224.585 51"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M32.433 123.874L32.433 138.454"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M32.433 164.806L32.433 179.386"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M149.024 101.496L149.024 125.096"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M156.049 178.387L156.049 201.986"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M217.85 101.496L217.85 125.096"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M279.648 178.389L279.648 201.988"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M279.648 123.385L279.648 137.965"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M336.193 101.731L346.703 122.862"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M336.193 180.62L346.703 201.751"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M211.024 177.496L211.024 201.096"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M225.474 177.496L225.474 201.096"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M110.36 51L124.94 51"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M48.583 151.496L63.164 151.496"
-            className="line"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M1.5 1.5H225.1V101.5H1.5z"
-            className="text-container"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M1.599 101.501H372.399V201.501H1.599z"
-            className="text-container"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M63.3 1.5L63.3 101.5"
-            className="line4 delay1"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M125.283 1.5L125.283 101.5"
-            className="line4"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M163.623 1.5L163.474 125.096"
-            className="line3 delay2"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M63.3 101.501L63.3 201.501"
-            className="line4 delay1"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M125.089 101.001L125.089 201.001"
-            className="line4"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M187.099 101.501L187.099 201.501"
-            className="line4 delay"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M248.899 101.626L248.899 201.626"
-            className="line4 delay2"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M310.697 101.501L310.697 201.501"
-            className="line4"
-          ></path>
-          <path
-            fill="none"
-            stroke="#132354"
-            strokeMiterlimit="10"
-            strokeWidth="3"
-            d="M94.232 127.972L94.232 174.973"
-            className="line4 delay"
-          ></path>
-        </svg>
-        <p className="occupation">Software Engineer</p>
-        {/*<p className="occupation">Certified Personal Trainer</p>
-        <p className="occupation">Doctor of Music</p>*/}
-        <div className="btn-container">
-          <div className="btn">
-            <p className="text">Resume</p>
+      <header>
+        <div className="name-container">
+          <svg
+            className="name"
+            xmlns="http://www.w3.org/2000/svg"
+            width="373.899"
+            height="203.001"
+            x="0"
+            y="0"
+            version="1.1"
+            viewBox="0 0 373.899 203.001"
+            xmlSpace="preserve"
+          >
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M39.4 41.066L63 41.066"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M39.352 61.066L62.951 61.066"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M94.05 21.11L94.05 35.69"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M94.05 78.4L94.05 101.5"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M193.809 27.871L193.809 74.871"
+              className="line2"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M194.051 51L224.585 51"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M32.433 123.874L32.433 138.454"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M32.433 164.806L32.433 179.386"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M149.024 101.496L149.024 125.096"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M156.049 178.387L156.049 201.986"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M217.85 101.496L217.85 125.096"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M279.648 178.389L279.648 201.988"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M279.648 123.385L279.648 137.965"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M336.193 101.731L346.703 122.862"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M336.193 180.62L346.703 201.751"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M211.024 177.496L211.024 201.096"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M225.474 177.496L225.474 201.096"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M110.36 51L124.94 51"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M48.583 151.496L63.164 151.496"
+              className="line"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M1.5 1.5H225.1V101.5H1.5z"
+              className="text-container"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M1.599 101.501H372.399V201.501H1.599z"
+              className="text-container"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M63.3 1.5L63.3 101.5"
+              className="line4 delay1"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M125.283 1.5L125.283 101.5"
+              className="line4"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M163.623 1.5L163.474 125.096"
+              className="line3 delay2"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M63.3 101.501L63.3 201.501"
+              className="line4 delay1"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M125.089 101.001L125.089 201.001"
+              className="line4"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M187.099 101.501L187.099 201.501"
+              className="line4 delay"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M248.899 101.626L248.899 201.626"
+              className="line4 delay2"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M310.697 101.501L310.697 201.501"
+              className="line4"
+            ></path>
+            <path
+              fill="none"
+              stroke="#132354"
+              strokeMiterlimit="10"
+              strokeWidth="3"
+              d="M94.232 127.972L94.232 174.973"
+              className="line4 delay"
+            ></path>
+          </svg>
+          <p className="occupation">Software Engineer</p>
+          {/*<p className="occupation">Certified Personal Trainer</p>
+          <p className="occupation">Doctor of Music</p>*/}
+          <div className="btn-container">
+            <div className="btn">
+              <p className="text">Resume</p>
+            </div>
           </div>
         </div>
-      </div>
+        <nav>
+          <span className="nav-link">
+            Projects
+          </span>
+          <span className="nav-link">
+            Bio
+          </span>
+          <span className="nav-link">
+            Skills
+          </span>
+          <span className="nav-link">
+            Contact
+          </span>
+        </nav>
+      </header>
+      <main>
+        <div id="projects">
+
+        </div>
+      </main>
     </div>
   );
 }
